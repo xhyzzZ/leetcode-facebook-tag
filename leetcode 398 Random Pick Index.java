5c4958481d5ab6cf11f8671a416044d6f3dcc0ac
Haoyu Xu <kobale@HaoyudeMacBook-Pro.local>
2019-01-22 21:46:19 -0500

    2019.01.22

---
+++ leetcode 398 Random Pick Index.java
@@ -0,0 +1,28 @@
+//leetcode 398 Random Pick Index
+
+/*
+time: O(n)
+space: O()
+*/
+
+class Solution {
+	int[] nums;
+    Random rnd;
+
+    public Solution(int[] nums) {
+        this.nums = nums;
+        this.rnd = new Random();
+    }
+    
+    public int pick(int target) {
+        int result = -1;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != target)
+                continue;
+            if (rnd.nextInt(++count) == 0)
+                result = i;
+        }
+        return result;
+    }
+}
