e4e5ed4315a182cac64699144950728ec6e1f21a
Haoyu Xu <kobale@HaoyudeMacBook-Pro.local>
2019-02-05 00:18:29 -0500

    2019.02.05

--- leetcode 315 Count of Smaller Numbers After Self.java
+++ leetcode 315 Count of Smaller Numbers After Self.java
@@ -1,53 +1,59 @@
 //leetcode 315 Count of Smaller Numbers After Self
 
 /*
-time: O(nlogn)
+time: O(n^2)
 space: O(k)
+
+Traverse from nums[len - 1] to nums[0], and build a binary search tree, which stores:
+
+val: value of nums[i]
+count: if val == root.val, there will be count number of smaller numbers on the right
+
 */
 
 public class Solution {
-    private static int lowbit(int x) { return x & (-x); }
-    class FenwickTree {    
-	    private int[] sums;    
-	    
-	    public FenwickTree(int n) {
-	      sums = new int[n + 1];
-	    }
-	 
-	    public void update(int i, int delta) {    
-	      while (i < sums.length) {
-	          sums[i] += delta;
-	          i += lowbit(i);
-	        }
-	    }
-	 
-	    public int query(int i) {       
-		    int sum = 0;
-		    while (i > 0) {
-		        sum += sums[i];
-		        i -= lowbit(i);
-		    }
-		    return sum;
-	    }    
-	}
-	  
 	public List<Integer> countSmaller(int[] nums) {
-	    int[] sorted = Arrays.copyOf(nums, nums.length);
-	    Arrays.sort(sorted);
-	    Map<Integer, Integer> ranks = new HashMap<>();
-	    int rank = 0;
-	    for (int i = 0; i < sorted.length; ++i)
-	        if (i == 0 || sorted[i] != sorted[i - 1])
-	        ranks.put(sorted[i], ++rank);
-	    
-	    FenwickTree tree = new FenwickTree(ranks.size());
-	    List<Integer> ans = new ArrayList<Integer>();
-	    for (int i = nums.length - 1; i >= 0; --i) {
-		    int sum = tree.query(ranks.get(nums[i]) - 1);      
-		    ans.add(tree.query(ranks.get(nums[i]) - 1));
-		    tree.update(ranks.get(nums[i]), 1);
-	    }
-	    
-	    Collections.reverse(ans);
-	    return ans;
+		List<Integer> res = new ArrayList<>();
+		if(nums == null || nums.length == 0) return res;
+		TreeNode root = new TreeNode(nums[nums.length - 1]);
+		res.add(0);
+		for(int i = nums.length - 2; i >= 0; i--) {
+			int count = insertNode(root, nums[i]);
+			res.add(count);
+		}
+		Collections.reverse(res);
+		return res;
 	}
+
+	public int insertNode(TreeNode root, int val) {
+		int thisCount = 0;
+		while(true) {
+			if(val <= root.val) {
+				root.count++;
+				if(root.left == null) {
+					root.left = new TreeNode(val); break;
+				} else {
+					root = root.left;
+				}
+			} else {
+				thisCount += root.count;
+				if(root.right == null) {
+					root.right = new TreeNode(val); break;
+				} else {
+					root = root.right;
+				}
+			}
+		}
+		return thisCount;
+	}
+}
+
+class TreeNode {
+	TreeNode left; 
+	TreeNode right;
+	int val;
+	int count = 1;
+	public TreeNode(int val) {
+		this.val = val;
+	}
+}
