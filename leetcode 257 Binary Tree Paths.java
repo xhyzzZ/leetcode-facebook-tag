dea64d15b850bdb596ecb202e9526e91b8b476ea
Haoyu Xu <xhy@bu.edu>
2019-08-01 16:43:20 +0800

    2019.08.01

--- leetcode 257 Binary Tree Paths.java
+++ leetcode 257 Binary Tree Paths.java
@@ -1,47 +1,54 @@
 //leetcode 257 Binary Tree Paths
 
 /*
-time: O()
-space: O()
+time: O(n)
+space: O(h)
 */
+recursion
 public class Solution {
 	//recursion
     public List<String> binaryTreePaths(TreeNode root) {
         List<String> res = new LinkedList<String>();
-
-        if(root == null) return res;
-        if(root.left == null && root.right == null) {
+        if (root == null) return res;
+        if (root.left == null && root.right == null) {
         	res.add(Integer.toString(root.val));
         	return res;
         }
-
-        for(String s : binaryTreePaths(root.left)) {
+        for (String s : binaryTreePaths(root.left)) {
         	res.add(Integer.toString(root.val) + "->" + s);
         }
-        for(String s : binaryTreePaths(root.right)) {
+        for (String s : binaryTreePaths(root.right)) {
         	res.add(Integer.toString(root.val) + "->" + s);
         }
         return res;
     }
-
-
-    public List binaryTreePaths(TreeNode root) {
+}
+/*
+time: O(n)
+space: O(h)
+*/
+dfs
+public class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
         List<String> res = new ArrayList<>();
-        if (root == null) return res;
-        dfs(root, res, "");
+        StringBuilder sb = new StringBuilder();
+        helper(res, root, sb);
         return res;
     }
-    /*
-	time: O(n)
-    */
-    private void dfs(TreeNode root, List ls, String accum) {
-        if (root == null) return;
-        accum += (accum.length() == 0 ? "" : "->") + root.val;
-        if (root.left == null && root.right == null) {
-            ls.add(accum);
+    
+    private void helper(List<String> res, TreeNode root, StringBuilder sb) {
+        if(root == null) {
             return;
         }
-        dfs(root.left, ls, accum);
-        dfs(root.right, ls, accum);
+        int len = sb.length();
+        sb.append(root.val);
+        if (root.left == null && root.right == null) {
+            res.add(sb.toString());
+        } else {
+            sb.append("->");
+            helper(res, root.left, sb);
+            helper(res, root.right, sb);
+        }
+        sb.setLength(len);
     }
 }
--- leetcode 270 Closest Binary Search Tree Value.java
+++ leetcode 270 Closest Binary Search Tree Value.java
@@ -9,8 +9,21 @@ public class Solution {
     public int closestValue(TreeNode root, double target) {
         int a = root.val;
         TreeNode kid = target < a ? root.left : root.right;
-        if(kid == null) return a;
+        if (kid == null) return a;
         int b = closestValue(kid, target);
         return Math.abs(a - target) < Math.abs(b - target) ? a : b;
     }
+}
+
+public class Solution {
+	public int closestValue(TreeNode root, double target) {
+	    int ret = root.val;   
+	    while (root != null) {
+	        if (Math.abs(target - root.val) < Math.abs(target - ret)) {
+	            ret = root.val;
+	        }      
+	        root = root.val > target ? root.left: root.right;
+	    }     
+	    return ret;
+	}
 }
