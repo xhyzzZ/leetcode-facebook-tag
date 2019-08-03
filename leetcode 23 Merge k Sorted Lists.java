3ec766fb562e24daf6b7d195d44f974bde0eef5e
xhyzzZ <xhy@bu.edu>
2019-08-03 23:04:01 +0800

    2019.08.03

--- leetcode 23 Merge k Sorted Lists.java
+++ leetcode 23 Merge k Sorted Lists.java
@@ -1,15 +1,18 @@
 //leetcode 23 Merge k Sorted Lists
 
+/*
+time: O(nlogk)k代表链表的个数
+space: O()
+*/
 
-//time: O(nlogk) k代表链表的个数
 public class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists == null || lists.length == 0) return null;
+        if (lists == null || lists.length == 0) return null;
         return sort(lists, 0, lists.length - 1);
     }
 
     public ListNode sort(ListNode[], int lo, int hi) {
-    	if(lo > hi) return ListNode[lo];
+    	if (lo > hi) return ListNode[lo];
     	int mid = (hi - lo) / 2 + lo;
     	ListNode l1 = sort(lists, lo, mid);
     	ListNode l2 = sort(lists, mid + 1, hi);
@@ -17,9 +20,9 @@ public class Solution {
     }
 
     public ListNode merge(ListNode l1, ListNode l2) {
-    	if(l1 == null) return l2;
-    	if(l2 == null) return l1;
-    	if(l1.val < l2.val) {
+    	if (l1 == null) return l2;
+    	if (l2 == null) return l1;
+    	if (l1.val < l2.val) {
     		l1.next = merge(l1.next, l2);
     		return l1;
     	}
