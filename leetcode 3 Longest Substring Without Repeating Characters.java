e750eecb0093dfc2f2ae247aa8012a7be2ab66d4
xhyzzZ <xhy@bu.edu>
2019-08-04 21:41:51 +0800

    2019.08.03

--- leetcode 3 Longest Substring Without Repeating Characters.java
+++ leetcode 3 Longest Substring Without Repeating Characters.java
@@ -5,6 +5,31 @@
 time: O(n)
 space: O(1)
 */
+
+public class Solution {
+    public int lengthOfLongestSubstring2(String s) {
+        int[] map = new int[128];
+        int start = 0, end = 0, maxLen = 0, counter = 0;
+
+        while (end < s.length()) {
+            char c1 = s.charAt(end);
+            if (map[c1] > 0) counter++;
+            map[c1]++;
+            end++;
+
+            while (counter > 0) {
+                char c2 = s.charAt(start);
+                if (map[c2] > 1) counter--;
+                map[c2]--;
+                start++;
+            }
+            maxLen = Math.max(maxLen, end - start);
+        }
+        return maxLen;
+    }
+}
+
+
 public class Solution {
 	//Hashmap
     public int lengthOfLongestSubstring(String s) {
@@ -20,23 +45,28 @@ public class Solution {
         }
         return max;
     }
-    //HashSet
+}
 
+    
+//HashSet
+public class Solution {
     //a b c a b c d e
     // j = 0, i = 3 -> j = 1
     public int lengthOfLongestSubstring(String s) {
-    	if (s.length() == 0 || s == null) return 0;
-    	HashSet<Character> set = new HashSet<>();
-    	int res = 0;
-    	for (int i = 0, j = 0; i < s.length(); i++) {
-    		if (set.contains(s.charAt(i))) {
-    			set.remove(s.charAt(j++));
-    		} else {
-    			set.add(s.charAt(i));
-    			res = Math.max(res, set.size());
-    		}
-    	}
-    	return res;
+        if (s.length() == 0 || s == null) return 0;
+        HashSet<Character> set = new HashSet<>();
+        int res = 0;
+        for (int i = 0, j = 0; i < s.length(); i++) {
+            if (set.contains(s.charAt(i))) {
+                set.remove(s.charAt(j++));
+            } else {
+                set.add(s.charAt(i));
+                res = Math.max(res, set.size());
+            }
+        }
+        return res;
     }
-
 }
+    
+    
+
--- leetcode 340 Longest Substring with At Most K Distinct Characters.java
+++ leetcode 340 Longest Substring with At Most K Distinct Characters.java
@@ -7,25 +7,24 @@ space: O(256)
 
 class Solution {
     public int lengthOfLongestSubstringKDistinct(String s, int k) {
-        int[] count = new int[256];     // there are 256 ASCII characters in the world
-    
-	    int i = 0;  // i will be behind j
-	    int num = 0;
-	    int res = 0;
-	    
-	    for (int j = 0; j < s.length(); j++) {
-	        if(count[s.charAt(j)]++ == 0) {    // if count[s.charAt(j)] == 0, we know that it is a distinct character
-	            num++;
-	        }
-	        while (num > k && i < s.length()) {     // sliding window
-	            count[s.charAt(i)]--;
-	            if (count[s.charAt(i)] == 0) { 
-	                num--;
-	            }
-	            i++;
-	        }
-	        res = Math.max(res, j - i + 1);
+    	if (s == null || s.length() == 0) return 0;
+	    int[] map = new int[256];
+	    int start = 0, end = 0, maxLen = Integer.MIN_VALUE, counter = 0;
+
+	    while (end < s.length()) {
+	      	char c1 = s.charAt(end);
+	      	if (map[c1] == 0) counter++;
+	      	map[c1]++;
+	      	end++;
+
+	      	while (counter > k) {
+		        char c2 = s.charAt(start);
+		        if (map[c2] == 1) counter--;
+		        map[c2]--;
+		        start++;
+	      	}
+	      	maxLen = Math.max(maxLen, end - start);
 	    }
-	    return res;
-    }
+	    return maxLen;
+  	}
 }
