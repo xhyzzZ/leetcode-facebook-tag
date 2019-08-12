8330161b94266a82f6b233505874e1e380f09898
xhyzzZ <xhy@bu.edu>
2019-08-12 21:40:27 +0800

    2019.08.12

--- leetcode 46 Permutations.java
+++ leetcode 46 Permutations.java
@@ -4,28 +4,83 @@
 /*
 time: O(n!) O(2^n * n)
 space: O(n)
-
+Permutations: [1,2,3] tempList of backtracking solution displayed here:
+Before removal: 1
+Before removal: 1 2
+Before removal: 1 2 3
+After removal: 1 2
+After removal: 1
+Before removal: 1 3
+Before removal: 1 3 2
+After removal: 1 3
+After removal: 1
+After removal:
+Before removal: 2
+Before removal: 2 1
+Before removal: 2 1 3
+After removal: 2 1
+After removal: 2
+Before removal: 2 3
+Before removal: 2 3 1
+After removal: 2 3
+After removal: 2
+After removal:
+Before removal: 3
+Before removal: 3 1
+Before removal: 3 1 2
+After removal: 3 1
+After removal: 3
+Before removal: 3 2
+Before removal: 3 2 1
+After removal: 3 2
+After removal: 3
+After removal:
 */
-
 public class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        if(nums.length == 0 || nums == null) return res;
+        if (nums.length == 0 || nums == null) return res;
         helper(res, new ArrayList<>(), nums);
         return res;
     }
 
-    public static void helper(List<List<Integer>> res, List<Integer> list, int[] nums) {
-    	if(list.size() == nums.length) {
-    		res.add(new ArrayList<>(list));
+    public static void helper(List<List<Integer>> res, List<Integer> tempList, int[] nums) {
+    	if (tempList.size() == nums.length) {
+    		res.add(new ArrayList<>(tempList));
     		return;
     	}
 
-    	for(int i = 0; i < nums.length; i++) {
-    		if(list.contains(nums[i])) continue; //O(n)
-    		list.add(nums[i]);
-    		helper(res, list, nums);
-    		list.remove(list.size() - 1);
+    	for (int i = 0; i < nums.length; i++) {
+    		if (tempList.contains(nums[i])) continue; //O(n)
+    		tempList.add(nums[i]);
+    		helper(res, tempList, nums);
+    		tempList.remove(tempList.size() - 1);
     	}
+    }
+}
+
+
+public class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums.length == 0 || nums == null) return res;
+        helper(res, new ArrayList<>(), new HashSet<>(), nums);
+        return res;
+    }
+
+    public static void helper(List<List<Integer>> res, List<Integer> tempList, Set<Integer> tempSet, int[] nums) {
+        if (tempSet.size() == nums.length) {
+            res.add(new ArrayList<>(tempList));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (tempSet.contains(nums[i])) continue; //O(1)
+            tempSet.add(nums[i]);
+            tempList.add(nums[i]);
+            helper(res, tempList, tempSet, nums);
+            tempSet.remove(tempList.get(tempList.size() - 1));
+            tempList.remove(tempList.size() - 1);
+        }
     }
 }
--- leetcode 47 Permutations II.java
+++ leetcode 47 Permutations II.java
@@ -10,18 +10,19 @@ space: O(n)
 public class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        if(nums.length == 0 || nums == null) return res;
+        if (nums.length == 0 || nums == null) return res;
         Arrays.sort(nums);
         helper(res, new ArrayList<>(), nums, new boolean[nums.length]);
         return res;
     }
 
     public void helper(List<List<Integer>> res, List<Integer> list, int[] nums, boolean[] used) {
-    	if(list.size() == nums.length) {
+    	if (list.size() == nums.length) {
     		res.add(new ArrayList<>(list));
     	}
-    	for(int i = 0; i < nums.length; i++) {
-    		if(used[i] || i > 0 || nums[i] == nums[i - 1] && !used[i - 1]) continue;
+    	for (int i = 0; i < nums.length; i++) {
+            if (used[i]) continue;
+    		if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
     		used[i] = true;
     		list.add(nums[i]);
     		helper(res, list, nums, used);
