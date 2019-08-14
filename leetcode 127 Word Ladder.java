f919ba6692d9ad5516ee872c756ca95bbe091432
xhyzzZ <xhy@bu.edu>
2019-08-14 22:26:02 +0800

    2019.08.14

--- leetcode 127 Word Ladder.java
+++ leetcode 127 Word Ladder.java
@@ -7,12 +7,12 @@ space: O(n)
 
 /*
 bidirectional BFS
+双向bfs，设计两个set存放两遍遍历的单词，遍历最短set中的每个单词
+对每个单词的每个字母进行替换，如果替换的字母在dict中，那么加入一个新的set中并赋值给原set
 */
 public class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Set<String> dict = new HashSet<>();
-	    for (String word : wordList) dict.add(word);
-	    
+        Set<String> dict = new HashSet<>(wordList);
 	    if (!dict.contains(endWord)) return 0;
 	    
 	    Set<String> q1 = new HashSet<>();
@@ -25,32 +25,34 @@ public class Solution {
 	    
 	    while (!q1.isEmpty() && !q2.isEmpty()) {
 	        ++steps;
-	      	//always expend the smaller queue first
-	        if(q1.size() > q2.size()) {
+	      	// always expend the smaller queue first
+	        if (q1.size() > q2.size()) {
 		        Set<String> tmp = q1;
 		        q1 = q2;
 		        q2 = tmp;
 	        }
-	      
-	        Set<String> q = new HashSet<>();
+	      	// 临时set
+	        Set<String> temp = new HashSet<>();
 	      
 	        for (String w : q1) {        
 	        	char[] chs = w.toCharArray();
-	        	for(int i = 0; i < l; ++i) {
+	        	for (int i = 0; i < l; ++i) {
 		          	char ch = chs[i];
-		          	for(char c = 'a'; c <= 'z'; ++c) {
-		            chs[i] = c;
-		            String t = new String(chs);         
-		            if (q2.contains(t)) return steps + 1;            
-		            if (!dict.contains(t)) continue;            
-		            dict.remove(t);        
-		            q.add(t);
+		          	for (char c = 'a'; c <= 'z'; ++c) {
+			            chs[i] = c;
+			            String t = new String(chs);         
+			            if (q2.contains(t)) return steps + 1;            
+			            if (!dict.contains(t)) continue;            
+			            dict.remove(t);        
+			            temp.add(t);
 	            	}
+	            	// 还原
 	          		chs[i] = ch;
 	        	}
 	      	}
-	      	q1 = q;
+	      	//赋值给q1
+	      	q1 = temp;
 	    }
-	    return 0
+	    return 0;
     }
 }
--- leetcode 91 Decode Ways.java
+++ leetcode 91 Decode Ways.java
@@ -7,45 +7,48 @@ space: O(n)
 */
 public class Solution {
     public int numDecodings(String s) {
-	       if(s == null || s.length == 0) return 0;
-	       int len = s.length();
-	       int[] dp = new int[len + 1];
-	       dp[0] = 1;
-	       dp[1] = s.charAt(0) != '0' ? 1 : 0;
-	       for(int i = 2; i <= len; i++) {
-	       	int first = Integer.valueOf(s.substring(i - 1, i));
-	       	int second = Integer.valueOf(s.substring(i - 2, i));
-	       	if(first >= 1 && first <= 9) {
-	       		dp[i] += dp[i - 1];
-	       	}
-	       	if(second >= 10 && second <= 26) {
-	       		dp[i] += dp[i - 2];
-	       	}
+	    if (s == null || s.length == 0) return 0;
+	    int len = s.length();
+        int[] dp = new int[len + 1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) != '0' ? 1 : 0;
+        for (int i = 2; i <= len; i++) {
+            int first = Integer.valueOf(s.substring(i - 1, i));
+       	    int second = Integer.valueOf(s.substring(i - 2, i));
+       	    if (first >= 1 && first <= 9) {
+       	        dp[i] += dp[i - 1];
+       	    }
+       	    if(second >= 10 && second <= 26) {
+       	        dp[i] += dp[i - 2];
+       	    }
         }
         return dp[len]; 
     }
-
+}
 
 /*
 time: O(n)
 space: O(1)
 c1代表整个位数能组成的，比如1234能组成的，c2代表123能组成的
 */
+public class Solution {
     public int numDecodings(String s) {
-        if(s == null || s.length() == 0 || s.charAt(0) == '0') return 0;
+        if (s == null || s.length() == 0 || s.charAt(0) == '0') return 0;
         int c1 = 1;
         int c2 = 1;
-        for(int i = 1; i < s.length(); i++) {
-        	if(s.charAt(i) == '0') {
-        		c1 = 0;
-        	}
-        	if(s.charAt(i - 1) == '1' || s.charAt(i - 1) == '2' && s.charAt(i) <= '6') {
-        		c1 = c1 + c2;
-        		c2 = c1 - c2;
-        	} else {
-        		c2 = c1;
-        	}
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == '0') {
+                c1 = 0;
+            }
+            if (s.charAt(i - 1) == '1' || s.charAt(i - 1) == '2' && s.charAt(i) <= '6') {
+                c1 = c1 + c2;
+                c2 = c1 - c2;
+            } else {
+                c2 = c1;
+            }
         }
         return c1;
     }
 }
+
+    
