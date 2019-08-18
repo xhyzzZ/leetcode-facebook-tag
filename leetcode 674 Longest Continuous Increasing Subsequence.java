60f1360fb617847d7e9a04356a0a61d547505102
xhyzzZ <xhy@bu.edu>
2019-08-18 22:36:15 +0800

    2019.08.18

--- leetcode 674 Longest Continuous Increasing Subsequence.java
+++ leetcode 674 Longest Continuous Increasing Subsequence.java
@@ -8,8 +8,8 @@ space: O(1)
 public class Solution {
     public int findLengthOfLCIS(int[] nums) {
         int res = 0, cnt = 0;
-        for(int i = 0; i < nums.length; i++) {
-            if(i == 0 || nums[i - 1] < nums[i]) res = Math.max(res, ++cnt);
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0 || nums[i - 1] < nums[i]) res = Math.max(res, ++cnt);
             else cnt = 1;
         }
         return res;
