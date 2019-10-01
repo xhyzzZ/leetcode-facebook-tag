b428e68644aa061bcff954d98d7bbdd813a92666
xhyzzZ <xhy@bu.edu>
2019-09-30 23:29:54 -0400

    2019.09.30

--- leetcode 20 Valid Parentheses.java
+++ leetcode 20 Valid Parentheses.java
@@ -6,16 +6,16 @@
 public class Solution {
 	public boolean isValid(String s) {
 
-		if(s == null || s.length() == 0) return true;
+		if (s == null || s.length() == 0) return true;
 		Stack<Character> stack = new Stack<Character>();
-		for(char c : s.toCharArray()) {
-			if(c == '(')
+		for (char c : s.toCharArray()) {
+			if (c == '(')
 				stack.push(')')
-			else if(c == '{')
+			else if (c == '{')
 				stack.push('}')
-			else if(c == '[')
+			else if (c == '[')
 				stack.push(']')
-			else if(stack.isEmpty() || stack.pop() != c)
+			else if (stack.isEmpty() || stack.pop() != c)
 				return false;
 		}
 		return stack.isEmpty();
