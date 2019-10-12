81dbe89853c84d8d562cd39bfca248b64cc7b6e1
xhyzzZ <xhy@bu.edu>
2019-10-12 18:38:04 -0400

    2019.10.12

--- leetcode 103 Binary Tree Zigzag Level Order Traversal.java
+++ leetcode 103 Binary Tree Zigzag Level Order Traversal.java
@@ -3,8 +3,42 @@
 
 /*
 time: O(n)
-space: O(n)
+space: O(h)
 */
+bfs
+public class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        boolean zigzag = false;
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int cnt = queue.size();
+            for (int i = 0; i < cnt; i++) {
+                TreeNode node = queue.poll();
+                if (zigzag) {
+                    level.add(0, node.val);
+                } else {
+                    level.add(node.val);
+                }
+                
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            res.add(level);
+            zigzag = !zigzag;
+        }
+        return res;
+    }
+}
+
+dfs
 public class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
@@ -13,11 +47,11 @@ public class Solution {
     }
 
     private void helper(List<List<Integer>> res, int level, TreeNode cur) {
-    	if(cur == null) return;
-    	if(res.size() <= level) {
+    	if (cur == null) return;
+    	if (res.size() <= level) {
     		res.add(new ArrayList<>());
     	}
-    	if(level % 2 == 0) {
+    	if (level % 2 == 0) {
     		res.get(level).add(cur.val);
     	} else {
     		res.get(level).add(0, cur.val);
--- leetcode 3 Longest Substring Without Repeating Characters.java
+++ leetcode 3 Longest Substring Without Repeating Characters.java
@@ -4,6 +4,9 @@
 /*
 time: O(n)
 space: O(1)
+int[26] for Letters 'a' - 'z' or 'A' - 'Z'
+int[128] for ASCII
+int[256] for Extended ASCII
 */
 
 public class Solution {
@@ -54,17 +57,19 @@ public class Solution {
     // j = 0, i = 3 -> j = 1
     public int lengthOfLongestSubstring(String s) {
         if (s.length() == 0 || s == null) return 0;
-        HashSet<Character> set = new HashSet<>();
-        int res = 0;
-        for (int i = 0, j = 0; i < s.length(); i++) {
-            if (set.contains(s.charAt(i))) {
-                set.remove(s.charAt(j++));
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            // try to extend the range [i, j]
+            if (!set.contains(s.charAt(j))) {
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
             } else {
-                set.add(s.charAt(i));
-                res = Math.max(res, set.size());
+                set.remove(s.charAt(i++));
             }
         }
-        return res;
+        return ans;
     }
 }
     
