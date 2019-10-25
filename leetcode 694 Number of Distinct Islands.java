ea7ecf7f041a4b3917792f72857920b9461032b9
xhyzzZ <xhy@bu.edu>
2019-10-25 19:53:49 -0400

    2019.10.25

--- leetcode 694 Number of Distinct Islands.java
+++ leetcode 694 Number of Distinct Islands.java
@@ -8,9 +8,9 @@ space: O(n)
 class Solution {
     public int numDistinctIslands(int[][] grid) {
         Set<String> set = new HashSet<>();
-	    for(int i = 0; i < grid.length; i++) {
-	        for(int j = 0; j < grid[0].length; j++) {
-	            if(grid[i][j] != 0) {
+	    for (int i = 0; i < grid.length; i++) {
+	        for (int j = 0; j < grid[0].length; j++) {
+	            if (grid[i][j] != 0) {
 	                StringBuilder sb = new StringBuilder();
 	                dfs(grid, i, j, sb, "o"); // origin
 	                set.add(sb.toString());
@@ -20,7 +20,7 @@ class Solution {
 	    return set.size();
 	}
 	private void dfs(int[][] grid, int i, int j, StringBuilder sb, String dir) {
-	    if(i < 0 || i == grid.length || j < 0 || j == grid[i].length || grid[i][j] == 0) return;
+	    if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == 0) return;
 	    sb.append(dir);
 	    grid[i][j] = 0;
 	    dfs(grid, i - 1, j, sb, "u");
