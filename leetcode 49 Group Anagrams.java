f269cae2ba21337464153a4d7eb43f2f435f6341
xhyzzZ <xhy@bu.edu>
2020-03-22 23:49:38 -0400

    2020.03.22

--- leetcode 49 Group Anagrams.java
+++ leetcode 49 Group Anagrams.java
@@ -4,20 +4,25 @@
 time: O(m * n)
 space: O(n)
 */
-public class Solution {
+class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs.length == 0) return new ArrayList();
         Map<String, List<String>> map = new HashMap<>();
-        for (String str : strs) {
-            int[] arr = new int[26];
-            for (int i = 0; i < str.length(); i++) { 
-                arr[str.charAt(i) - 'a']++;
+        int[] count = new int[26];
+        for (String s : strs) {
+            Arrays.fill(count, 0);
+            for (char c : s.toCharArray()) count[c - 'a']++;
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 26; i++) {
+                sb.append('#');
+                sb.append(count[i]);
             }
-            String key = Arrays.toString(arr);
-            List<String> tempList = map.getOrDefault(key, new LinkedList<>());
-            tempList.add(str);
-            map.put(key, tempList);
+            String key = sb.toString();
+            if (!map.containsKey(key)) map.put(key, new ArrayList());
+            map.get(key).add(s);
         }
-        return new LinkedList<>(map.values());
+        return new ArrayList(map.values());
     }
 }
 
