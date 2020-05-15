1be5b2bd98d044020ac9154acc667a8ab7576044
xhyzzZ <xhy@bu.edu>
2020-05-15 15:27:45 -0400

    2020.05.15

---
+++ 1401-1500/leetcode 1424 Diagonal Traverse II.java
@@ -0,0 +1,28 @@
+// leetcode 1424 Diagonal Traverse II
+
+/*
+time: O(n)
+space: O(n)
+*/
+
+class Solution {
+    public int[] findDiagonalOrder(List<List<Integer>> nums) {
+        int n = 0, i = 0, maxKey = 0;
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int r = nums.size() - 1; r >= 0; --r) { // The values from the bottom rows are the starting values of the diagonals.
+            for (int c = 0; c < nums.get(r).size(); ++c) {
+                map.putIfAbsent(r + c, new ArrayList<>());
+                map.get(r + c).add(nums.get(r).get(c));
+                maxKey = Math.max(maxKey, r + c);
+                n++;
+            }
+        }
+        int[] ans = new int[n];
+        for (int key = 0; key <= maxKey; ++key) {
+            List<Integer> value = map.get(key);
+            if (value == null) continue;
+            for (int v : value) ans[i++] = v;
+        }
+        return ans;
+    }
+}
