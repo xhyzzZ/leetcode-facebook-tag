33d26aef5b8b8ea7dfa28859acd67b801fcd6c76
Haoyu Xu <41977460+xhyzzZ@users.noreply.github.com>
2021-11-16 23:15:05 -0500

    2021.11.16

--- 101-200/leetcode 101 Symmetric Tree.java
+++ 101-200/leetcode 101 Symmetric Tree.java
@@ -13,8 +13,8 @@ public class Solution {
     }
 
     public boolean helper(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
     	if (p == null || q == null) return false;
-    	if (p == null && q == null) return true;
     	return (p.val == q.val) && helper(p.left, q.right) & helper(p.right, q.left);
     }
 }
--- 501-600/leetcode 572 Subtree of Another Tree.java
+++ 501-600/leetcode 572 Subtree of Another Tree.java
@@ -1,22 +1,22 @@
 //leetcode 572 Subtree of Another Tree
 
 /*
-time: O(2^n)
+time: O(mn)
 space: O(h)
 */
 
 class Solution {
     public boolean isSubtree(TreeNode s, TreeNode t) {
-        if(s == null) return false;
-        if(isSame(s, t)) return true;
+        if (s == null) return false;
+        if (isSame(s, t)) return true;
         return isSubtree(s.left, t) || isSubtree(s.right, t);
     }
     
     private boolean isSame(TreeNode s, TreeNode t) {
-        if(s == null && t == null) return true;
-        if(s == null || t == null) return false;
+        if (s == null && t == null) return true;
+        if (s == null || t == null) return false;
         
-        if(s.val != t.val) return false;
+        if (s.val != t.val) return false;
         
         return isSame(s.left, t.left) && isSame(s.right, t.right);
     }
