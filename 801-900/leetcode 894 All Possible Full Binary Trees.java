cee4ae30ae112e0417248087f5d19bb3d72837ea
Haoyu Xu <41977460+xhyzzZ@users.noreply.github.com>
2021-11-18 22:29:13 -0500

    2021.11.18

---
+++ 801-900/leetcode 894 All Possible Full Binary Trees.java
@@ -0,0 +1,33 @@
+// leetcode 894 All Possible Full Binary Trees
+
+/*
+time: O(2^n)
+space: O(2^n)
+*/
+
+class Solution {
+    Map<Integer, List<TreeNode>> memo = new HashMap();
+
+    public List<TreeNode> allPossibleFBT(int n) {
+        if (!memo.containsKey(n)) {
+            List<TreeNode> ans = new LinkedList();
+            if (n == 1) {
+                ans.add(new TreeNode(0));
+            } else if (n % 2 == 1) {
+                for (int x = 0; x < n; ++x) {
+                    int y = n - 1 - x;
+                    for (TreeNode left : allPossibleFBT(x))
+                        for (TreeNode right: allPossibleFBT(y)) {
+                            TreeNode bns = new TreeNode(0);
+                            bns.left = left;
+                            bns.right = right;
+                            ans.add(bns);
+                        }
+                }
+            }
+            memo.put(n, ans);
+        }
+
+        return memo.get(n);
+    }
+}
