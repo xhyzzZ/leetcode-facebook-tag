93eb917dab539feee06b68da341a925af3bae603
Haoyu Xu <41977460+xhyzzZ@users.noreply.github.com>
2021-11-23 20:29:02 -0500

    2021.11.23

--- 101-200/leetcode 153 Find Minimum in Rotated Sorted Array.java
+++ 101-200/leetcode 153 Find Minimum in Rotated Sorted Array.java
@@ -1,26 +1,69 @@
-//leetcode 153 Find Minimum in Rotated Sorted Array
+// leetcode 153 Find Minimum in Rotated Sorted Array
 
 /*
 time: O(logn)
 space: O(1)
 */
-// [2,4,5,6,7,0,1]
+
 public class Solution {
 	public int findMin(int[] nums) {
-		if (num == null || num.length == 0) return 0;
-        if (num.length == 1) return num[0];
-        int start = 0, end = num.length - 1;
-        while (start < end) {
-            int mid = (start + end) / 2;
-            if (mid > 0 && num[mid] < num[mid - 1]) {
-                return num[mid];
-            }
-            if (num[start] <= num[mid] && num[mid] > num[end]) {
-                start = mid + 1;
+		if (nums == null || nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+        int left = 0, right  = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) {
+                // we KNOW the pivot must be to the right of the middle:
+                // if nums[mid] > nums[right], we KNOW that the
+                // pivot/minimum value must have occurred somewhere to the right
+                // of mid, which is why the values wrapped around and became smaller.
+
+                // example:  [3,4,5,6,7,8,9,1,2] 
+                // in the first iteration, when we start with mid index = 4, right index = 9.
+                // if nums[mid] > nums[right], we know that at some point to the right of mid,
+                // the pivot must have occurred, which is why the values wrapped around
+                // so that nums[right] is less then nums[mid]
+
+                // we know that the number at mid is greater than at least
+                // one number to the right, so we can use mid + 1 and
+                // never consider mid again; we know there is at least
+                // one value smaller than it on the right
+                left = mid + 1;
             } else {
-                end = mid - 1;
+                // here, nums[mid] <= nums[right]:
+                // we KNOW the pivot must be at mid or to the left of mid:
+                // if nums[mid] <= nums[right], we KNOW that the pivot was not encountered
+                // to the right of middle, because that means the values would wrap around
+                // and become smaller (which is caught in the above if statement).
+                // this leaves the possible pivot point to be at index <= mid.
+                            
+                // example: [8,9,1,2,3,4,5,6,7]
+                // in the first iteration, when we start with mid index = 4, right index = 9.
+                // if nums[mid] <= nums[right], we know the numbers continued increasing to
+                // the right of mid, so they never reached the pivot and wrapped around.
+                // therefore, we know the pivot must be at index <= mid.
+
+                // we know that nums[mid] <= nums[right].
+                // therefore, we know it is possible for the mid index to store a smaller
+                // value than at least one other index in the list (at right), so we do
+                // not discard it by doing right = mid - 1. it still might have the minimum value.
+                right = mid;
             }
         }
-        return num[start];
+
+        // at this point, left and right converge to a single index (for minimum value) since
+        // our if/else forces the bounds of left/right to shrink each iteration:
+
+        // when left bound increases, it does not disqualify a value
+        // that could be smaller than something else (we know nums[mid] > nums[right],
+        // so nums[right] wins and we ignore mid and everything to the left of mid).
+
+        // when right bound decreases, it also does not disqualify a
+        // value that could be smaller than something else (we know nums[mid] <= nums[right],
+        // so nums[mid] wins and we keep it for now).
+
+        // so we shrink the left/right bounds to one value,
+        // without ever disqualifying a possible minimum
+        return nums[left];
 	}
 }
--- 501-600/leetcode 540 Single Element in a Sorted Array.java
+++ 501-600/leetcode 540 Single Element in a Sorted Array.java
@@ -1,23 +1,23 @@
-//leetcode 540 Single Element in a Sorted Array
+// leetcode 540 Single Element in a Sorted Array
 
 /*
-time: O(n)
+time: O(logn)
 space: O(1)
 */
 
 class Solution {
     public int singleNonDuplicate(int[] nums) {
         // corner case
-		if(nums == null || nums.length == 0) return -1;
-		
+		if (nums == null || nums.length == 0) return -1;
+		// 只有可能偶数是答案 nums[0], nums[2]...
 		int lo = 0;
 		int hi = nums.length - 1;
-		while(lo < hi) {
+		while (lo < hi) {
 			int mid = lo + (hi - lo) / 2;
 			// trick here
-			// int temp = mid % 2 == 0 ? mid + 1: mid - 1;
-			int temp = mid ^ 1; // if even, mid + 1; if odd, mid - 1
-			if(nums[mid] == nums[temp]) {
+			// if even, mid + 1; if odd, mid - 1
+			int temp = mid % 2 == 0 ? mid + 1 : mid - 1;
+			if (nums[mid] == nums[temp]) {
 				// if mid is even, then nums[mid] = nums[mid + 1], single number is >= mid + 2
 				// if mid is odd, then nums[mid] = nums[mid - 1], single number is >= mid + 1
 				// so we choose mid + 1
